d1c7ef841dfd3ff56baf4652a230d94915d16cfd
ppoddo <lhj950720@naver.com>
2024-03-19 19:53:06 +0900

    "master..noop"

--- zloa_project/src/main/java/com/doubleH/zloa/character/controller/CharacterController.java
+++ zloa_project/src/main/java/com/doubleH/zloa/character/controller/CharacterController.java
@@ -1,30 +1,69 @@
 package com.doubleH.zloa.character.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.doubleH.zloa.api.model.service.LostArkApiClient;
 import com.doubleH.zloa.character.model.vo.Character;
 
-@Controller
+@RestController
+@RequestMapping("/character")
 public class CharacterController {
-
 	
-	@PostMapping("/processData")
-	public ResponseEntity<String> processData(@RequestBody Character requestData) {
-        // YourDataClass: 클라이언트가 전송하는 JSON 데이터에 대한 모델 클래스
+	@Autowired
+	private final LostArkApiClient lostArkClient;
+	
+	@Autowired
+    public CharacterController(LostArkApiClient lostArkClient) {
+		this.lostArkClient = lostArkClient;
+    }
+	
+    @GetMapping("/api/data")
+    public ResponseEntity<String> getData(@RequestParam("id") String id) {
+        // url을 고정하는 대신 RequestParam으로 파라미터로써 매개변수를 받음
+        // ex) location.href="/api/data?id='끄구마'"
 
-        // 데이터 처리 로직 수행
-        String processedData = processDataLogic(requestData);
+        String responseData = id + " 초월했나요?";
+        System.out.println(id);
 
-        // 처리된 데이터를 JSON 형태로 클라이언트에게 반환
-        return ResponseEntity.ok(processedData);
+        // 반환할 데이터는 ResponseEntity가 제공하는 ok메소드의 인자에 담으면 되고
+        // 이 때, 인자에 담는 데이터의 형태에 따라 제네릭 바꿔주기^^
+        return ResponseEntity.ok(responseData);
     }
 
-    private String processDataLogic(Character requestData) {
-        // 데이터 처리 로직 구현
-        // 예시: 간단히 받은 문자열을 대문자로 변환
-        return requestData.getData().toUpperCase();
+    @PostMapping("/api/data")
+    public ResponseEntity<String> getData1(@RequestBody Character ch) {
+        // url을 고정하는 대신 RequestParam으로 파라미터로써 매개변수를 받음
+        // ex) location.href="/api/data?id='끄구마'"
+
+        String responseData = ch.getData() + " Post?";
+        String responseData1 = ch.getData1() + " Post?";
+        System.out.println(responseData);
+        System.out.println(responseData1);
+
+        // 반환할 데이터는 ResponseEntity가 제공하는 ok메소드의 인자에 담으면 되고
+        // 이 때, 인자에 담는 데이터의 형태에 따라 제네릭 바꿔주기^^
+        return ResponseEntity.ok(responseData);
     }
+    
+    @GetMapping("/characters/siblings")
+    public ResponseEntity<String> getCharacterSiblings(@RequestParam("characterName") String characterName) {
+
+    	String i = lostArkClient.getCharacterSiblings(characterName);
+    	System.out.println(i);
+    	
+    	// 성공!
+        return ResponseEntity.ok(i);
+    }
+    
+    
+    
+    
+    // https://developer-lostark.game.onstove.com/characters/{characterName}/siblings
 }
